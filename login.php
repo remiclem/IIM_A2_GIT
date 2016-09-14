e0cfce4ffd1dfa8d4d9d97188ffca395ffa17df2
Jules Castex <julesctx@MacBook-Pro-de-Jules-3.local>
2016-09-14 18:05:26 +0200

    mdr

--- login.php
+++ login.php
@@ -17,7 +17,7 @@ if(isset($_POST['email']) && isset($_POST['password'])){
 		// TODO
 
 		// Force user connection to access dashboard
-		userConnection($db, 'git@initiation.com', 'password');
+		userConnection($db,$_POST['email'],$_POST['password']);
 		
 		header('Location: dashboard.php');
 
